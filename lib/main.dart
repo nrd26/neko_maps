04a1d328ecfd3112828d5122e4f0179e6beaa191
Nihal Rian Dias <nihaldias261@gmail.com>
2023-07-12 19:09:35 +0530

    Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,35 @@ class MyApp extends StatelessWidget {
             centerTitle: true,
             backgroundColor:Colors.black),
         drawer: Drawer(
-            child: Container()// Populate the Drawer in the next step.
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                ),
+                child: Text('Drawer Header', style: TextStyle(color: Colors.white),),
+              ),
+              ListTile(
+                title: const Text('Item 1'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
+                title: const Text('Item 2'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+            ],
+          ),
         ),
         body: FlutterMap(
           options: MapOptions(
